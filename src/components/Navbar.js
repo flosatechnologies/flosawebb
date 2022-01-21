37f5153a95f013c700b5aa08b081a23bc37f4b97
iamkupoe <samuelkupoe1@gmail.com>
2022-01-20 20:45:24 -0600

    Navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/Navbar.css";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import logo from "../images/logo.jpg";
+import { Phone, Instagram, Facebook } from "react-bootstrap-icons";
+
+export default class NavbarMenu extends Component {
+  render() {
+    return (
+      <div id="home" className="pt-5 mr-20px">
+        <Navbar bg="black" expand="md" className="navbar fixed-top">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                width="100px"
+                height="auto"
+                className="img-responsive"
+                src={logo}
+                alt="logo"
+              />
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="navbarScroll" className="bg-light" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="justify-content-center"
+                style={{ width: "100%", height: "50%", color: "white" }}
+                navbarScroll
+              >
+                <Nav.Link href="#home">HOME</Nav.Link>
+                <Nav.Link href="#skills">ABOUT</Nav.Link>
+                <Nav.Link href="#skills">SERVICES</Nav.Link>
+                <Nav.Link href="#projects">PROJECTS</Nav.Link>
+                <Nav.Link href="#contact">CONTACT</Nav.Link>
+
+                <ul id="cssnav" className="navright"></ul>
+
+                <div className="social-side">
+                  <h5>
+                    <a
+                      href="https://www.facebook.com/search/top?q=flosa%20technologies"
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{ color: "#007785", textDecoration: "none" }}
+                    >
+                      <li>
+                        <Facebook
+                          color="blue"
+                          size={20}
+                          paddingTop={5}
+                          marginRight={20}
+                        />
+                      </li>
+                    </a>
+                  </h5>
+
+                  <h5>
+                    <a
+                      href="https://www.instagram.com/flosatech/"
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{ color: "#E1306C", textDecoration: "none" }}
+                    >
+                      <li>
+                        <Instagram
+                          color="#F56040"
+                          size={20}
+                          paddingTop={5}
+                          marginRight={30}
+                        />
+                      </li>
+                    </a>
+                  </h5>
+
+                  <h5>
+                    <a
+                      href="tel:+233546835848"
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{
+                        color: "white",
+                        textDecoration: "none",
+                        paddingRight: "30px",
+                      }}
+                    >
+                      <li>
+                        <Phone color="white" size={20} />
+                      </li>
+                    </a>
+                  </h5>
+                </div>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
