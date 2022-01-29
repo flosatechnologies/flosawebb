a44d4032ce7e4d2dc10ea717ca1733277a42f6dd
iamkupoe <samuelkupoe1@gmail.com>
2022-01-29 17:23:44 -0600

    Update: Tech component added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import FLOSA from "../components/FLOSA";
+import Technologies from './../components/Technologies';
 
 class About extends Component {
   render() {
     return <div>
     <FLOSA/>
-    
+    <Technologies/>
     </div>;
   }
 }
