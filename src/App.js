da8537ea3c676c4a25b04a5ededebd15b382b6e7
Adams <bamfoadamsf@gmail.com>
2022-02-21 22:46:37 +0000

    started working on the project page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import NavbarMenu from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <Container fluid={true} className="App">
       <NavbarMenu />
+      <Projects />
       <Footer />
     </Container>
   );
