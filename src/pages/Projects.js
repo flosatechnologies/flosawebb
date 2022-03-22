443603303522f65d841e3294a3f9ea945e3cbff7
Adams <bamfoadamsf@gmail.com>
2022-03-22 23:06:52 +0000

    minor post merge touches

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,40 +1,28 @@
-
-import React from "react";
+import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 import { projectList } from "../data.js";
-import Project from "./Project";
-
-const Projects = () => {
-  return (
-    <Container>
-      <div id="projects" className="pt-5">
-        <h2 className="align-items-center text-center p-5 ">Projects</h2>
-        <div>
-          {projectList.map((project) => (
-            <Project
-              key={project.id}
-              image={project.image}
-              text={project.text}
-              button={project.button}
-            />
-          ))}
-        </div>
-      </div>
-    </Container>
-  );
-};
-=======
-import React, { Component } from "react";
+import Project from "../components/Project";
 
 export class Projects extends Component {
   render() {
     return (
-      <div>
-        <h1>This is the project Page</h1>
-      </div>
+      <Container>
+        <div id="projects" className="pt-5">
+          <h2 className="align-items-center text-center p-5 ">Projects</h2>
+          <div>
+            {projectList.map((project) => (
+              <Project
+                key={project.id}
+                image={project.image}
+                text={project.text}
+                button={project.button}
+              />
+            ))}
+          </div>
+        </div>
+      </Container>
     );
   }
 }
-
 
 export default Projects;
