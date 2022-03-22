fb893d12349a04561f133aa66b666211cc87cece
Adams <bamfoadamsf@gmail.com>
2022-03-22 23:05:41 +0000

    minor post merge touches

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import "./App.css";
 import NavbarMenu from "./components/Navbar";
-
-import Hero from "./pages/Hero";
-import Projects from "./pages/Projects";
-import Services from "./pages/Services";
-
-function App() {
-  return (
-    <div className="app">
-      <NavbarMenu />
-
-      <Services />
-      <Projects />
-=======
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import RouterComponent from "./components/Router";
 
-
 function App() {
   return (
     <div className="App">
-
       <RouterComponent />
-
-
     </div>
   );
 }
